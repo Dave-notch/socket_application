09a7897facfb2095150f9b558940d344164e5456
dave-notch <stsaamir86@gmail.com>
2026-05-16 16:42:12 +0300

    feat: added mobile version to the frontend

--- fr_main.js
+++ fr_main.js
@@ -1,13 +1,34 @@
+
+const toggle = document.querySelector(".menu-toggle");
+const links = document.querySelector(".nav-links");
+
+toggle.addEventListener("click", () => {
+links.classList.toggle("active");
+  });
+
 let SendBtn=document.getElementById("SendBtn")
 let input_area=document.getElementById("input_area")
 SendBtn.addEventListener("click",()=>{
 let in_area=input_area.value
 let el=document.createElement("div")
-el.className="flex justify-end p-2 bg-green-200 sooceli"
+el.className="flex justify-end p-2 bg-green-200 w-50 label corner rounded flex-col gap-1 rounded-tr-none label2 sooceli innerGR"
+let el2=document.createElement("div")
+let el3=document.createElement("p")
+el3.className="text-sm text-green-700 border-t flex label2 justify-end"
+el.appendChild(el2)
+el.appendChild(el3)
+const time = new Date().toLocaleTimeString([],{
+    hour: "2-digit",
+    minute: "2-digit"
+})
+el3.innerHTML=time
+
+
 
     if(in_area){
-    el.innerHTML=in_area
+    el2.innerHTML=in_area
     document.getElementById("messeges").appendChild(el)
+    input_area.value=""
     }
 
 })
